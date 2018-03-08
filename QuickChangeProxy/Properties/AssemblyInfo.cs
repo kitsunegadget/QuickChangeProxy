8a503792034da11714fda37bc339e5aa5e6a3c94
yuzusiro <kitsunegadget@gmail.com>
2018-03-08 10:29:06 +0900

    Version edit

--- QuickChangeProxy/Properties/AssemblyInfo.cs
+++ QuickChangeProxy/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("QuickChangeProxy")]
-[assembly: AssemblyCopyright("Copyright ©  2016  hitakiyu, Garakuta Gadget")]
+[assembly: AssemblyCopyright("Copyright ©  2016  YuY, Garakuta Gadget")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
